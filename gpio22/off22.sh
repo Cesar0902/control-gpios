277d64f767c9263ac959886b7ac51c9e0bb16e09
Cesar0902 <cescobarp@unah.hn>
2024-12-09 09:45:47 -0600

    Scripts para actualizar el estado de gpio22 en la BD

---
+++ gpio22/off22.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Cambiar el estado del GPIO
+echo 0 > /home/cesar/estado22.txt
+
+# Configuración de la base de datos
+DB_HOST="localhost"
+DB_USER="developer"
+DB_PASS="Developer"
+DB_NAME="arquitectura"
+DB_TABLE="proyecto_final"
+
+# Insertar registro en la base de datos
+mysql -h "$DB_HOST" \
+      -u "$DB_USER" \
+      -p"$DB_PASS" \
+      -D "$DB_NAME" \
+      -e "
+       INSERT INTO $DB_TABLE (desc_gpios, estado, fecha) 
+       VALUES ('GPIO22', '0', NOW());
+       "
+
+echo "GPIO 22 APAGADO"
