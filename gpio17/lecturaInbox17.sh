10d210f209154e0b9e7969744323d1230f18f5f5
Cesar0902 <cescobarp@unah.hn>
2024-12-09 14:00:34 -0600

    Lectura inbox solo lee una vez

--- gpio17/lecturaInbox17.sh
+++ gpio17/lecturaInbox17.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-while true
-	  do
-	     valor1=$(python3 readinbox.py | grep -E "ON|on|On|oN" | cut -d " " -f3 | cut -b 24-25)
-	     valor2=$(python3 readinbox.py | grep -E "OFF|off|Off|oFf|ofF|OFf|oFF|OfF" | cut -d " " -f3 | cut -b 24-26)
+last_processed=""
 
-	     if [[ $valor1 = "ON" || $valor1 = "on" || $valor1 = "On" || $valor1 = "oN" ]]
-				  then
-				      echo $valor1
-	 			      sudo /./home/cesar/on17.sh
-	     else
-	         if [[ $valor2 = "OFF" || $valor2 = "off" || $valor2 = "Off" || $valor2 = "oFf" || $valor2 = "ofF" || $valor2 = "OFf" || $valor2 = "oFF" || $valor2 = "OfF" ]]
-				       then
-				 	   echo $valor2
-					   sudo /./home/cesar/off17.sh
-				       fi
-	     fi
-	     sleep 1
-	  done
+while true
+do
+    # Leer el correo solo una vez
+    correo=$(python3 readinbox.py)
+
+    # Filtrar "ON" y "OFF" del correo
+    valor1=$(echo "$correo" | grep -E "ON|on|On|oN" | cut -d " " -f3 | cut -b 24-25)
+    valor2=$(echo "$correo" | grep -E "OFF|off|Off|oFf|ofF|OFf|oFF|OfF" | cut -d " " -f3 | cut -b 24-26)
+
+    # Si el valor del correo es "ON" y no ha sido procesado antes
+    if [[ -n "$valor1" && ( "$valor1" == "ON" || "$valor1" == "on" || "$valor1" == "On" || "$valor1" == "oN" ) && "$correo" != "$last_processed" ]]
+    then
+        echo "Encendiendo GPIO17"
+        sudo /./home/cesar/on17.sh
+        last_processed="$correo"  # Marcar correo como procesado
+    elif [[ -n "$valor2" && ( "$valor2" == "OFF" || "$valor2" == "off" || "$valor2" == "Off" || "$valor2" == "oFf" || "$valor2" == "ofF" || "$valor2" == "OFf" || "$valor2" == "oFF" || "$valor2" == "OfF" ) && "$correo" != "$last_processed" ]]
+    then
+        echo "Apagando GPIO17"
+        sudo /./home/cesar/off17.sh
+        last_processed="$correo"  # Marcar correo como procesado
+    fi
+    
+    sleep 1
+done
